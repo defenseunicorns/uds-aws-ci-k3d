f3604ac5d68e345c5286b05f7168014dad4c3291
Zachariah Miller <zachariah.miller@defenseunicorns.com>
2023-10-04 13:51:40 -0400

    revert testing changes. fix k3d/k3s flag not working

--- terraform/create-cluster.sh
+++ terraform/create-cluster.sh
@@ -42,7 +42,6 @@ client_ip="$(curl -s "https://checkip.amazonaws.com")"
 terraform init -backend-config="key=uds-aws-ci-k3d/${ID}.tfstate"
 checkError "terraform"
 
-echo "The value for env var K3S equals: $K3S"
 
 terraform plan -var="client_ip=$client_ip" -var="suffix=${ID}" \
     -var="instance_size=${INSTANCE_SIZE}" -var="k3d_config=${K3D_CONFIG}" \
